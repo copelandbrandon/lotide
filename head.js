0eb69b158638e003b275718f47438be08dfb5fea
Brandon Copeland <copelandbrandon2@gmail.com>
2021-04-26 22:13:47 +0000

    created a function that retrieves the first item in an array while checking against a separate function

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array){
+  return array[0];
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 6)
+assertEqual(head([8]), 8)
