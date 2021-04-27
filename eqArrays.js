844ef88b2ee7712dac413071c0d8840fbd087b6d
Brandon Copeland <copelandbrandon2@gmail.com>
2021-04-27 23:19:24 +0000

    created a function that compares two arrays and returns either true or false depending on if they are exactly the same

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//need to compare two arrays for a perfect match
+// take two arrays
+//loop through both arrays and compare similar indexes
+//use assertequal to test the functionality by writing test cases
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length > array2.length || array2.length > array1.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        if (array1[i] === array1.length) {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+
+};
+
+
+assertEqual(eqArrays([1, 1, 3], [1, 1, 1, 1]), false);// => should PASS
+assertEqual(eqArrays([1, 2, 4], [1, 4, 2]), false);// => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);// => should PASS
+assertEqual(eqArrays(['1', '2', '3'], [1, 2, 3]), false);// => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 8]), true);// => should FAIL
+assertEqual(eqArrays([5, 6, 7, 8, 9,], ['5', '6', 7, 8, 'nine']), true);// => should FAIL
