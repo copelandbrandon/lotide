8d07b8202bc539292d9ac28e6e050e5277d6346f
Brandon Copeland <copelandbrandon2@gmail.com>
2021-04-28 22:37:43 +0000

    created function that returns the first key of a given object that contains the given value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑️🛑️🛑️Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+//takes in an object and a value
+//scan the object 
+//return the first key which contains the value
+//if no key contains the value return undefined
+const findKeyByValue = function(object, val){
+  for (let key in object){
+    if (object[key] === val){
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'), 'comedy')
