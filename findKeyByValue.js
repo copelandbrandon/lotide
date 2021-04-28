a4ce16694311d1b5eecb5c4f92c3ed6f328e31aa
Brandon Copeland <copelandbrandon2@gmail.com>
2021-04-28 22:42:29 +0000

    linted code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,18 +6,18 @@ const assertEqual = function(actual, expected) {
   }
 };
 //takes in an object and a value
-//scan the object 
+//scan the object
 //return the first key which contains the value
 //if no key contains the value return undefined
-const findKeyByValue = function(object, val){
-  for (let key in object){
-    if (object[key] === val){
+const findKeyByValue = function(object, val) {
+  for (let key in object) {
+    if (object[key] === val) {
       return key;
     }
   }
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
@@ -25,4 +25,4 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'), 'comedy')
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'Brooklyn Nine-Nine'), 'comedy');
