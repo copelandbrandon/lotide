8b1ed737dc3fc7d7e44b67b66bb98bbb3c4026b1
Brandon Copeland <copelandbrandon2@gmail.com>
2021-04-29 01:45:13 +0000

    resolved hoisting issue that appeared when using this function with another

--- eqArrays.js
+++ eqArrays.js
@@ -14,20 +14,15 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
 
-  if (array1.length > array2.length || array2.length > array1.length) {
+  if (array1.length !== array2.length) {
     return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        if (array1[i] === array1.length) {
-          return true;
-        }
-      } else {
+  } 
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
         return false;
-      }
     }
   }
-
+  return true;
 };
 
 
