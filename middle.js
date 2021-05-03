a4f8d26bd862c0e9ff73f7b1aa6209c1f39d0e93
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-03 19:50:06 +0000

    refactored to remove some redundant code

--- middle.js
+++ middle.js
@@ -2,7 +2,7 @@ const middle = function(array){
   if (array.length <= 2){
     return [];
   } else if (array.length % 2 === 0){
-    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+    return [array[(array.length / 2) - 1], array[array.length / 2]];
   }else {
     return [array[Math.floor(array.length / 2)]];
   }
