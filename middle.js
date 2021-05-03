adf94277716de79c3826abbb6c52115d14b56650
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-03 19:47:10 +0000

    refactored code to export into an appropriate test file

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i += 1) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-
-//define a middle function
 const middle = function(array){
   if (array.length <= 2){
     return [];
@@ -29,12 +8,10 @@ const middle = function(array){
   }
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);//should PASS
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);//should PASS
-assertArraysEqual(middle([1]), []);//should PASS
-assertArraysEqual(middle([1, 2]), []);//should PASS
-//take in an array
-//find the middle element/s
-//arrays with 1 or 2 elements do not have a middle and should return empty arrays
-//arrays with odd number of elements should return an array of 1 element
-//arrays with even number of elements should return an array with 2 elements
+// prior testing
+// assertArraysEqual(middle([1, 2, 3]), [2]);//should PASS
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);//should PASS
+// assertArraysEqual(middle([1]), []);//should PASS
+// assertArraysEqual(middle([1, 2]), []);//should PASS
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const middle = require('../middle')
+const assertArraysEqual = require('../assertArraysEqual'); 
+
+assertArraysEqual(middle([1, 2, 3]), [2]);//should PASS
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);//should PASS
+assertArraysEqual(middle([1]), []);//should PASS
+assertArraysEqual(middle([1, 2]), []);//should PASS
