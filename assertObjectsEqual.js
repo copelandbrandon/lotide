8c4d8a6b29298b64e3e88f43e72db2c9c6e31d6a
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-04 00:45:41 +0000

    refactored to export and import code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,4 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const eqObjects = function(obj1, obj2) {
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  }
-  for (let key in obj1) {
-      
-    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-
-      let result = eqArrays(obj1[key], obj2[key]);
-      return result;
-    } else if (obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-    
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -42,3 +12,5 @@ const assertObjectsEqual = function(actual, expected) {
 assertObjectsEqual({a: 1, b: 2, c:3}, {a: 1, b: 2, c:3});//should PASS
 assertObjectsEqual({a: 2, b: 3, c: 4}, {a: 5, b: 6, c: 7});//should FAIL
 assertObjectsEqual({a: 1, b: 2, c:[3, 2]}, {a: 1, b: 2, c:[3, 2]});//should PASS
+
+module.exports = assertObjectsEqual;
--- eqObjects.js
+++ eqObjects.js
@@ -1,27 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅️✅️✅️Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑️🛑️🛑️Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-//take in two objects
-//return boolean value based on perfect match
-// objects are equal when they have same # of keys
-//& value of key in one object is the same as value of key in the other
 const eqObjects = function(obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) {
     return false;
@@ -40,10 +19,6 @@ const eqObjects = function(obj1, obj2) {
   return true;
 };
 
-
-
-
-
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
@@ -59,3 +34,5 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+module.exports = eqObjects;
